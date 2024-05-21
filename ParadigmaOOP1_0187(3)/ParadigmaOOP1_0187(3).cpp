1a94c8af2eeabcb986434869dccde6de79ee1fc8
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2024-05-21 12:02:17 +0700

    anak

--- ParadigmaOOP1_0187(3)/ParadigmaOOP1_0187(3).cpp
+++ ParadigmaOOP1_0187(3)/ParadigmaOOP1_0187(3).cpp
@@ -7,5 +7,21 @@ using namespace std;
 
 int main()
 {
-    
+    ibu* varIbu = new ibu("duta");
+    ibu* varIbu2 = new ibu("asep");
+    anak* varAnak1 = new anak("decul");
+    anak* varAnak2 = new anak("ariel");
+    anak* varAnak3 = new anak("nugroho");
+
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+    varIbu->tambahAnak(varAnak3);
+    varIbu2->tambahAnak(varAnak2);
+
+    delete varIbu;
+    delete varIbu2;
+    delete varAnak1;
+    delete varAnak2;
+    delete varAnak3;
 }
+   
